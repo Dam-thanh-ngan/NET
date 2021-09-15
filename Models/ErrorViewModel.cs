a1095bf31272f334e64d42ad32b1fee3ba2324a5
NGAN DAM THANH <1821050429@student.humg.edu.vn>
2021-09-15 10:26:27 +0700

    lan1

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace thmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
